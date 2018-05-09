0c27bc26ecc49d977abb95247d844e4748451aab
Боря <borya.bobroff2014@@yandex.ru>
2018-05-09 11:50:14 +0300

    First commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Боря
+ * Date: 09.05.2018
+ * Time: 10:03
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function admin()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+}
